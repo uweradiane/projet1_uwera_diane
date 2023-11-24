22c0597aaaa0e37d843c5a93cab90eca01e1d0bb
uweradiane <uweradiane27@gmail.com>
2023-11-24 13:15:27 -0500

    page principal

---
+++ page.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="page.css" />
+<?php
+echo"<form>";
+echo"<fieldset>";
+echo "<legend for='text'>Diane Fashion Design</legend>";
+echo "<a href='index.php'>Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+echo "<a href='clientinfo.php'>Client</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+echo "<a href='Admin'>Admin</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+echo "<a href='Products'>Products</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+echo"</field>";
+echo"</form>";
+?>
